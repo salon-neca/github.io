c8072e644ec47ec335c9f1221f9c4cd67f3e8b33
Glitch (s25) <none>
2018-08-14 08:31:48 +0000

    25z

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-  var versionCode= 'v0.25y Aug\'18. \n';
+  var versionCode= 'v0.25z Aug\'18. \n';
   var appPath= 'https://snn.glitch.me';
   $.ajaxSetup({async:true, cache:false, timeout:9999});
 
