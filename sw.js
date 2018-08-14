8ff7c6c34f4cf87826304c3ef5ff646615eb4d42
Glitch (s25) <none>
2018-08-14 09:53:56 +0000

    25z7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// *** [18j].[24g]25hijklmnopquvwxy[25z]2345
+// *** [18j].[24g]25hijklmnopquvwxy[25z]23457
 self.addEventListener('install', e => {
   caches.delete('saloncch').then(cache => {
       caches.open('saloncch').then(cache => {
