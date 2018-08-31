c9c8c4fa70dda69519e4781eb96a756f28403e1c
Glitch (s31) <none>
2018-08-31 07:47:59 +0000

    31a

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-  var versionCode= 'v0.31h Aug\'18. \n';
+  var versionCode= 'v0.31i Aug\'18. \n';
   var appPath= 'https://snn.glitch.me';
   $.ajaxSetup({async:true, // dataType:'text',
                contentType:'text/plain; charset=utf-8', cache:false, timeout:19999});
