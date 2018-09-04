b8c4cba7083a0884e0340e788f78b5ef2c4da2f1
Glitch (s32) <none>
2018-09-04 04:14:16 +0000

    32h

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-  var versionCode= 'v32g Aug\'18. \n';
+  var versionCode= 'v32h Aug\'18. \n';
   var appPath= 'https://snn.glitch.me';
   $.ajaxSetup({async:true, // dataType:'text',
                contentType:'text/plain; charset=utf-8', cache:false, timeout:19999});
